0c4f94e875574928e4fc1efeaf853e222372d6ee
Job Gardiner <jobgardner@gmail.com>
2025-07-20 18:22:24 +0300

    Fix: Use views.register to match checker format

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register, homepage
+from . import views
+#from .views import list_books, LibraryDetailView, register, homepage
 
 urlpatterns = [
-    path('', homepage, name='home'),
-    path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('', views.homepage, name='home'),
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Auth URLs
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
