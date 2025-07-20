9f3147811838fd59d8f881447a74c26f4704f322
Job Gardiner <jobgardner@gmail.com>
2025-07-20 20:15:02 +0300

    Add role-based views and URL routing for Admin, Librarian, and Member

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from .models import Book
 from .models import Library
 
