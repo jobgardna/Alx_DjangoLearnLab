39d4f1cd2eedf8b63d59f0efe737ecd5b10a3d1e
Job Gardiner <jobgardner@gmail.com>
2025-07-20 20:48:59 +0300

    Add permission_required decorators to secure book-related views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
 from .models import Book
 from .models import Library
 
